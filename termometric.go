8121682a311a6b7fd2266d10770a13f9f0876369
Willian Simas <163364952+simasguga@users.noreply.github.com>
2024-11-28 17:35:32 -0300

    Update termometric.go

--- termometric.go
+++ termometric.go
@@ -6,5 +6,5 @@ const ebu_k float64 = 373.15
 
 func main() {
 	conv := ebu_k - 273
-	fmt.Printf("Temperature conversion = %.2f to = %.2f Cº\n", ebu_k, conv)
+	fmt.Printf("Temperature in Kelvin: %.2f K\n Temperature in Celsius: %.2f Cº\n", ebu_k, conv)
 }
